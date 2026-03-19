454a65db492c311191b1ea2be3afc11c80083690
christianMDG <christian.ravelojaona@student.passerellesnumeriques.org>
2026-03-19 22:55:30 +0300

    chore(configuration maven)

---
+++ src/main/java/com/Christian/spring_http_handler/SpringHttpHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.Christian.spring_http_handler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHttpHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHttpHandlerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Christian/spring_http_handler/SpringHttpHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Christian.spring_http_handler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHttpHandlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
