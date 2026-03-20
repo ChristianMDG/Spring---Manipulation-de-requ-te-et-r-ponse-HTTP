321b022b1f5e256d8eed4a613929129f7b579fab
christianMDG <christian.ravelojaona@student.passerellesnumeriques.org>
2026-03-20 14:59:49 +0300

    feat(get welcome with status)

--- src/main/java/com/Christian/spring_http_handler/controller/StudentController.java
+++ src/main/java/com/Christian/spring_http_handler/controller/StudentController.java
@@ -1,5 +1,23 @@
 package com.Christian.spring_http_handler.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class StudentController {
 
+    @GetMapping("/welcome")
+    public ResponseEntity<String> welcome(@RequestParam(value = "name", required = false) String name){
+        if (name==null || name.trim().isEmpty()){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Parameter 'name' is required");
+        }
+        else {
+            String reponse = "Welcome "+name+"!";
+            return ResponseEntity.status(HttpStatus.OK).body(reponse);
+        }
+    }
 }
--- src/main/java/com/Christian/spring_http_handler/service/StudentService.java
+++ src/main/java/com/Christian/spring_http_handler/service/StudentService.java
@@ -1,4 +1,8 @@
 package com.Christian.spring_http_handler.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class StudentService {
+
 }
