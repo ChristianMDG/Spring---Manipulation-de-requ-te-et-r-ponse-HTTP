c67d1dca204611d836dad345f16f410b5d4d09ea
christianMDG <christian.ravelojaona@student.passerellesnumeriques.org>
2026-03-20 15:35:16 +0300

    feat(post student with errorhandler)

--- src/main/java/com/Christian/spring_http_handler/controller/StudentController.java
+++ src/main/java/com/Christian/spring_http_handler/controller/StudentController.java
@@ -1,15 +1,20 @@
 package com.Christian.spring_http_handler.controller;
 
+import com.Christian.spring_http_handler.entity.Student;
+import com.Christian.spring_http_handler.service.StudentService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class StudentController {
-
+ private StudentService studentService =  new StudentService();
     @GetMapping("/welcome")
     public ResponseEntity<String> welcome(@RequestParam(value = "name", required = false) String name){
         if (name==null || name.trim().isEmpty()){
@@ -18,6 +23,16 @@ public class StudentController {
         else {
             String reponse = "Welcome "+name+"!";
             return ResponseEntity.status(HttpStatus.OK).body(reponse);
+        }
+    }
+
+    @PostMapping("/students")
+    public ResponseEntity<List<Student>> addStudents(@RequestBody List<Student> students){
+        try{
+            List<Student> savedStudents = studentService.addStudent(students);
+            return ResponseEntity.status(HttpStatus.CREATED).body(savedStudents);
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
 }
--- src/main/java/com/Christian/spring_http_handler/service/StudentService.java
+++ src/main/java/com/Christian/spring_http_handler/service/StudentService.java
@@ -1,8 +1,22 @@
 package com.Christian.spring_http_handler.service;
 
+import com.Christian.spring_http_handler.entity.Student;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class StudentService {
-
+    List<Student> students = new ArrayList<>();
+    public List<Student> addStudent(List<Student> newStudents){
+        if (newStudents== null || newStudents.isEmpty()) {
+            throw new RuntimeException("Erreur serveur simulée");
+        }
+        students.addAll(newStudents);
+        return students;
+    }
+    public List<Student> getStudents(){
+       return students;
+    }
 }
